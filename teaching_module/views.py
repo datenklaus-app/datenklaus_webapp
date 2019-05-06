485da5abfa7b55e71a455d714e0d87ac8a446c00
bontric <benjohnwie@gmail.com>
2019-05-06 19:58:26 +0200

    update

--- teaching_module/views.py
+++ teaching_module/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
-
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return render(request, 'teaching_module/index.html')
+    template = loader.get_template('teaching_module/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
