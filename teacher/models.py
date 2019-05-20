48b7324495fbf6e6dac711f6436dff43842aaadb
goapunk <goapunk@riseup.net>
2019-05-20 17:45:56 +0200

    adjust room model

--- teacher/models.py
+++ teacher/models.py
@@ -1,10 +1,13 @@
 from django.contrib.sessions.models import Session
 from django.db import models
+from django.db.models import CASCADE, ForeignKey, CharField
 
 
 class Room(models.Model):
-    room_name = models.CharField(max_length=4096)
-    clients = models.ForeignKey(Session)
+    room_name = models.CharField(max_length=4096, primary_key=True)
 
-    def __str__(self):
-        return self.room_name
+
+class Client(models.Model):
+    user_name = CharField(max_length=4)
+    session = ForeignKey(Session, on_delete=CASCADE)
+    room = ForeignKey(Room, on_delete=CASCADE)
--- teacher/views.py
+++ teacher/views.py
@@ -1,15 +1,25 @@
+from django.contrib.sessions.models import Session
 from django.shortcuts import render
+
+from teacher.models import Client, Room
 
 
 def index(request):
     from teacher import random_word_chain
     context = {'random_room': random_word_chain.random_word_chain(), }
-    return render(request, 'teaching_module/index.html', context=context)
+    return render(request, 'teacher/index.html', context=context)
 
 
 def room(request, room_name):
     if request.session.get("room") == "":
         request.session["room"] = room_name
-
-    context = {'room_name': room_name}
-    return render(request, 'teaching_module/room.html', context=context)
+    created = Room.objects.get_or_create(room_name=room_name)[1]
+    clients = {} if created else Client.objects.filter(room=room_name)
+    data = {}
+    for client in clients:
+        data["name"] = client.user_name
+        s = Session.objects.get(client.session)
+        data["session"] = s.session_key
+        data["last_activity"] = s.expire_date
+    context = {'room_name': room_name, 'clients': data}
+    return render(request, 'teacher/room.html', context=context)
