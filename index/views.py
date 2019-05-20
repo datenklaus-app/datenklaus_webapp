cd0411e6fbacc67fd282e0acc2f2b0a517619af1
bontric <benjohnwie@gmail.com>
2019-05-20 20:06:54 +0200

    Make sure session is stored initially

--- index/views.py
+++ index/views.py
@@ -3,4 +3,7 @@ import json
 from django.shortcuts import render
 
 def index(request):
+    # TODO: check for existing session
+    # Make sure session Id is assigned
+    request.session.save()
     return render(request, 'index/index.html')
