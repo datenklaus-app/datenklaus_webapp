ce5b3db32a3e75065f96a31263191485fdfc91e2
goapunk <goapunk@riseup.net>
2019-05-21 19:47:48 +0200

    Update client list in teacher view

--- teacher/views.py
+++ teacher/views.py
@@ -1,4 +1,5 @@
 from django.contrib.sessions.models import Session
+from django.http import JsonResponse
 from django.shortcuts import render
 
 from teacher import constants
@@ -15,14 +16,17 @@ def room(request, room_name):
     if request.session.get("room") == "":
         request.session["room"] = room_name
     created = Room.objects.get_or_create(room_name=room_name)[1]
-    clients = {} if created else Client.objects.filter(room=room_name)
+    if created or not request.is_ajax():
+        context = {'room_name': room_name}
+        return render(request, 'teacher/teacher_room.html', context=context)
+    clients = Client.objects.filter(room=room_name)
     clients_info = []
     for client in clients:
         s = Session.objects.get(session_key=client.session)
         decoded = s.get_decoded()
         module_state = decoded.get("module_state", constants.MODULE_STATE_WAITING)
-        clients_info.append(
-            (client.user_name, s.session_key, module_state, s.expire_date))
-    context = {'room_name': room_name,
-               'clients_info': clients_info}
-    return render(request, 'teacher/teacher_room.html', context=context)
+        clients_info.append({"name": client.user_name,
+                             "session": s.session_key,
+                             "progress": module_state,
+                             "expiry": s.expire_date})
+    return JsonResponse({"clients": clients_info})
