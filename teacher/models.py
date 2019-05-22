145f9c5b9a49d25f472156a748c0525ae9cd57c0
bontric <benjohnwie@gmail.com>
2019-05-22 17:32:47 +0200

    use handlebars instead of jsrender

--- teacher/models.py
+++ teacher/models.py
@@ -5,7 +5,7 @@ from django.db.models import CASCADE, ForeignKey, CharField
 
 class Room(models.Model):
     room_name = models.CharField(max_length=4096, primary_key=True)
-    module = models.CharField(max_length=4096)
+    #module = models.CharField(max_length=4096)
 
     def __str__(self):
         return self.room_name
