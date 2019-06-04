71ff54c02df019ddddf6ba4ba1ba9151ac01d382
goapunk <goapunk@riseup.net>
2019-06-04 18:13:54 +0200

    add module selection to rooms

--- teacher/views.py
+++ teacher/views.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from lessons.views import get_all
+from lessons.views import get_lessons_list, get_lessons_description
 from teacher.models import Room
 from teacher.utils import get_students_for_room
 
@@ -15,10 +15,26 @@ def index(request):
 def room(request, room_name):
     if request.session.get("room") == "":
         request.session["room"] = room_name
-    created = Room.objects.get_or_create(room_name=room_name)[1]
+    buf = Room.objects.get_or_create(room_name=room_name)
+    r = buf[0]
+    created = buf[1]
     if created or not request.is_ajax():
-        modules = get_all()
+        mod = get_lessons_list()
+        modules = []
+        for m in mod:
+            modules.append({'name': m, 'description': get_lessons_description(m)})
         context = {'room_name': room_name, 'modules': modules}
         return render(request, 'teacher/teacher_room.html', context=context)
-    students = get_students_for_room(room_name)
-    return JsonResponse({"students": students})
+    rtype = request.POST.get('type', None)
+    if rtype is None:
+        raise ConnectionError
+    if rtype == "students":
+        students = get_students_for_room(room_name)
+        return JsonResponse({"students": students})
+    else:
+        module = request.POST.get("module", None)
+        if module is None or module not in get_lessons_list():
+            return JsonResponse({"error": "module not found"})
+        else:
+            r.module = module
+            return JsonResponse({"error": "none"})
