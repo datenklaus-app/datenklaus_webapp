f9eacf414cb09a820664ef8845a23c06c029a0f9
bontric <benjohnwie@gmail.com>
2019-06-04 16:39:31 +0200

    update lessons api

--- lessons/views.py
+++ lessons/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
 from lessons.teaching_modules import internet
 from lessons.teaching_modules.teaching_module import STATE_INITIAL
+
 
 def get_content(lesson, state, arguments):
     if lesson == "INTERNET":
@@ -11,5 +10,10 @@ def get_content(lesson, state, arguments):
         else:
             return internet.next(state, arguments)
 
-def get_all():
+
+def get_lessons_list():
     return ["INTERNET"]
+
+
+def get_lessons_description(lesson):
+    return "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
