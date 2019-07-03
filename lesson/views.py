f4f2a16dbd23f30eae6fd3e9bab3d7a355f5e58d
bontric <benjohnwie@gmail.com>
2019-07-03 17:06:33 +0200

    Make this a little nicer

--- lesson/views.py
+++ lesson/views.py
@@ -1,22 +1,23 @@
 # Create your views here.
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
-from lesson.lessons.card import Card
 from lesson.lessons.cardJsonParser import from_json
 from lesson.lessons.internet import Internet
 from student.models import Student, CardState
 
 STATE_INITIAL = 0
 
-def get_initial(lesson):
-    if lesson == "INTERNET":
+
+def get_initial(lesson_name):
+    if lesson_name == "INTERNET":
         return Internet.initial()
     raise NotImplementedError("Selected Lesson does not exist")
 
 
-def get_content(lesson, state, arguments):
-    if lesson == "INTERNET":
+def get_content(lesson_name, state, arguments):
+    if lesson_name == "INTERNET":
         return Internet.next(state, arguments)
     raise NotImplementedError("Selected Lesson does not exist")
 
@@ -37,20 +38,19 @@ def lesson(request):
     context = {"rname": student.room}
 
     current_lesson = student.room.module
-    cards = CardState.objects.filter(student=student,
-                                     state=student.current_state,
-                                     lesson=current_lesson)
-    if len(cards) == 1:
-        card = from_json(cards.first().card)
-        card.handle_post(request)
-        student.current_state = card.get_next_state()
-        next_state_args = card.get_args()
-        card = get_content(current_lesson, student.current_state, next_state_args)
-        CardState.objects.get_or_create(student=student,
-                                        lesson=current_lesson,
-                                        state=student.current_state,
-                                        card=card.to_json())
-    else:
+    try:
+        card_obj = CardState.objects.get(student=student, state=student.current_state, lesson=current_lesson)
+        card = from_json(card_obj.card)
+        if len(request.POST) != 0:
+            card.handle_post(request)
+            student.current_state = card.get_next_state()
+            next_state_args = card.get_args()
+            card = get_content(current_lesson, student.current_state, next_state_args)
+            CardState.objects.get_or_create(student=student,
+                                            lesson=current_lesson,
+                                            state=student.current_state,
+                                            card=card.to_json())
+    except ObjectDoesNotExist:
         card = get_initial(current_lesson)
         CardState.objects.get_or_create(student=student,
                                         lesson=current_lesson,
