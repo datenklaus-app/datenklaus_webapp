dbbb423bc93dc1289ea9501e837a0dd242e8a406
goapunk <goapunk@riseup.net>
2019-07-04 15:31:29 +0200

    rework room creation and room layout

--- teacher/static/teacher/js/rango-jquery.js
+++ teacher/static/teacher/js/rango-jquery.js
@@ -7,6 +7,21 @@ $(document).ready(function () {
         } else {
             room_name = $('#room_name').val();
         }
-        window.location.href = 'room/' + room_name;
+        let lesson = $('#modules').find('button.active').find('#module_name').data('name')
+        myRedirect('room/' + room_name, 'lesson', lesson)
     });
+
+    $("button.list-group-item").click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+
+
 });
+
+myRedirect = function (redirectUrl, arg, value) {
+    const form = $('<form action="' + redirectUrl + '" method="post">' + CSRF_TOKEN +
+        '<input type="hidden" + " name="' + arg + '" value="' + value + '">' + '</form>');
+    $('body').append(form);
+    console.log(form)
+    $(form).submit();
+}
--- teacher/views.py
+++ teacher/views.py
@@ -2,40 +2,38 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 from lesson.views import get_lessons_list, get_lessons_description
+from teacher import random_word_chain
 from teacher.models import Room
 from teacher.utils import get_students_for_room
 
 
 def index(request):
-    from teacher import random_word_chain
-    context = {'random_room': random_word_chain.random_word_chain(), }
+    mod = get_lessons_list()
+    for i in range(0, 10):
+        mod.append(mod[0] + str(i))
+    modules = []
+    for m in mod:
+        modules.append({'name': m, 'description': get_lessons_description(m)})
+    context = {'random_room': random_word_chain.random_word_chain(), 'modules': modules}
     return render(request, 'teacher/index.html', context=context)
 
 
 def room(request, room_name):
     if request.session.get("room") == "":
         request.session["room"] = room_name
-    buf = Room.objects.get_or_create(room_name=room_name)
+    if request.is_ajax():
+        return refresh_student_list(room_name)
+    lesson = request.POST.get("lesson", None)
+    # TODO: Error Handling
+    #    if lesson is None or lesson not in get_lessons_list():
+    #       return HttpResponseBadRequest('Invalid Lesson')
+    buf = Room.objects.get_or_create(room_name=room_name, module=lesson)
     r = buf[0]
-    created = buf[1]
-    if created or not request.is_ajax():
-        mod = get_lessons_list()
-        modules = []
-        for m in mod:
-            modules.append({'name': m, 'description': get_lessons_description(m)})
-        context = {'room_name': room_name, 'modules': modules}
-        return render(request, 'teacher/teacher_room.html', context=context)
-    rtype = request.POST.get('type', None)
-    if rtype is None:
-        raise ConnectionError
-    if rtype == "students":
-        students = get_students_for_room(room_name)
-        return JsonResponse({"students": students})
-    else:
-        module = request.POST.get("module", None)
-        if module is None or module not in get_lessons_list():
-            return JsonResponse({"error": "module not found"})
-        else:
-            r.module = module
-            r.save()
-            return JsonResponse({"error": "none"})
+    context = {'room_name': room_name, 'module': r.module}
+    return render(request, 'teacher/teacher_room.html', context=context)
+
+
+def refresh_student_list(room_name):
+    r = Room.objects.get_or_create(room_name=room_name)[0]
+    students = get_students_for_room(r)
+    return JsonResponse({"students": students})
