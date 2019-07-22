98478f7543e4855e2fa07a411aa1c45ec1a84035
bontric <benjohnwie@gmail.com>
2019-07-22 13:13:54 +0200

    update bar chart card

--- lesson/cards/BarChartCard.py
+++ lesson/cards/BarChartCard.py
@@ -1,4 +1,3 @@
-import chartjs
 import pygal
 from django.shortcuts import render
 from pygal.style import DefaultStyle
@@ -6,29 +5,31 @@ from pygal.style import DefaultStyle
 from lesson.cards.card import Card
 
 
-class LineChartCard(Card):
+class BarChartCard(Card):
 
-    def __init__(self, labels, title="Ergebnisse", colors=chartjs.colors.COLORS):
+    def __init__(self, state, labels, title="Ergebnisse"):
         """
         :param labels: List of labels
         :param title: Card title
         :param colors: Optional color list
         """
-        self.colors = colors
+        self.state = state
         self.title = title
         self.labels = labels
 
     def render(self, request, context, dataset=[]):
         style = DefaultStyle()
         style.background = "#FFFFFF"
-        chart = pygal.Bar(show_legend=False, show_y_labels=True, classes=(..., "card-img-bottom", "img-fluid"),
-                          height=400, explicit_size=True)
+        chart = pygal.Bar(show_legend=False, show_y_labels=True,
+                          classes=(..., "card-img-bottom", "img-fluid"),
+                          style=style, height=450)
         chart.x_labels = self.labels
         chart.y_labels = range(max(dataset) + 1)
 
         chart.add('', dataset)
         context["title"] = self.title
         context["cht"] = chart.render(is_unicode=True)
+        context["state"] = self.state
         return render(request, 'lessons/cards/barChartCard.html', context=context)
 
     def handle_post(self, post):
--- lesson/internet/bState.py
+++ lesson/internet/bState.py
@@ -1,14 +1,21 @@
-from lesson.cards.BarChartCard import LineChartCard
+from lesson.cards.BarChartCard import BarChartCard
 from lesson.internet.aState import AState
+from lesson.internet.states import CSTATE, BSTATE
 from lesson.lessonState import LessonState
+from lesson.models import LessonSateModel
 from student.models import Student
 
 
 class BState(LessonState):
-    card = LineChartCard(list(map(lambda i: str(i), range(1, 11))), title="So schätzt ihr euch ein:")
+    def get_state_number(self):
+        return BSTATE
+
+    card = BarChartCard(BSTATE,
+                        list(map(lambda i: str(i), range(1, 11))),
+                        title="So schätzt ihr euch ein:")
 
     def next_state(self, student: Student) -> int:
-        return 0  # FIXME: Implement more states
+        return CSTATE
 
     def render(self, request, student: Student, context) -> str:
         results = AState().get_results(student.room)
