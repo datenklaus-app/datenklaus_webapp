888b83ea3199fe2aacddef7e9409937108fd502c
goapunk <goapunk@riseup.net>
2019-07-22 20:06:38 +0200

    [teacher] show actual student progress

--- teacher/utils.py
+++ teacher/utils.py
@@ -4,17 +4,23 @@ from enum import Enum
 from django.contrib.sessions.models import Session
 from django.http import JsonResponse, HttpResponse
 
+from lesson.lessonUtil import get_lesson
 from student.models import Student
 from teacher.constants import LESSON_STATE_WAITING
+from teacher.models import Room
 
 
 def get_students_for_room(room_name):
+    try:
+        room = Room.objects.get(room_name=room_name)
+        lesson = get_lesson(room.lesson)
+    except Room.DoesNotExist as e:
+        raise e
     students = Student.objects.filter(room=room_name)
     student_info = []
     for student in students:
         s = Session.objects.get(session_key=student.session)
-        decoded = s.get_decoded()
-        lesson_state = decoded.get("lesson_state", LESSON_STATE_WAITING)
+        lesson_state = LESSON_STATE_WAITING if room.state == -1 else lesson.get_state(student.current_state).get_name()
         student_info.append({"name": student.user_name,
                              "session": s.session_key,
                              "progress": lesson_state,
