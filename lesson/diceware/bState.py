921f55efca3b45007be33810dcecd17ccdc28a2d
bontric <benjohnwie@gmail.com>
2019-08-13 18:40:26 +0200

    fix astate update bstate layout

--- lesson/diceware/bState.py
+++ lesson/diceware/bState.py
@@ -5,16 +5,7 @@ from student.models import Student
 
 
 class BState(LessonState):
-    card = TextCard(state=BSTATE, title="Spielprinzip", subtitle="",
-                    text="Für das Erstellen eines sicheren Passworts ist vor allem eins wichtig: Der Zufall. Und wie "
-                         "du vielleicht weißt, ist das Ergebnis eines ungezinkten (d.h. nicht manipulierten) "
-                         "Würfelwurfs immer zufällig - denn es steht nicht zu Beginn fest, wieviele Augen angezeigt "
-                         "werden. Diesen Zufall machen wir uns hier zunutze. Mithilfe von fünf Würfeln und einer "
-                         "langen Wortliste können wir uns ans Werk machen. In der Liste findest du alle würfelbaren "
-                         "Zahlenkombinationen (wir haben nachgezählt: 7776). Du würfelst also fünf Würfel - heraus "
-                         "kommt eine zufällige Zahlenkombination, die für ein bestimmtes Wort steht, das du dir "
-                         "notierst. Das Ganze wiederholst du fünfmal und am Ende hast du fünf zufällig gewürfelte "
-                         "Wörter, eine sogenannte Passphrase, die du als Passwort verwenden kannst.")
+    card = TextCard(None, None, None, state=BSTATE,template="lesson/diceware/b-textCard.html")
 
     def state_number(self) -> int:
         return BSTATE
@@ -30,4 +21,4 @@ class BState(LessonState):
 
     @staticmethod
     def name():
-        return "Selbsteinschätzung: Ergebnis"
+        return "Erklärung: Spielprinzip"
