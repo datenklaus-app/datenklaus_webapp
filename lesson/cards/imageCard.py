2c6a7bdeb8f284a5d5f4b3e8596263b8b3c4533c
bontric <benjohnwie@gmail.com>
2019-08-13 14:56:00 +0200

    Fix image card in single image scenario and add optional title

--- lesson/cards/imageCard.py
+++ lesson/cards/imageCard.py
@@ -5,16 +5,18 @@ from lesson.cards.card import Card
 
 
 class ImageCard(Card):
-    def __init__(self, images, state, template='lesson/cards/imageCard.html'):
+    def __init__(self, images, state, template='lesson/cards/imageCard.html', title=None):
         self.images = images
         self.state = state
         self.template = template
+        self.title = title
 
     def render(self, request, context):
         form = ImageCardForm()
         context["images"] = self.images
         context["form"] = form
         context["state"] = self.state
+        context["title"] = self.title
         return render(request, self.template, context=context)
 
     def post(self, post):
