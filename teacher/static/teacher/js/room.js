4f4f77aef4ddbd948ad377c1b23f1285e97a596f
goapunk <goapunk@riseup.net>
2019-08-16 18:04:23 +0200

    fix js

--- teacher/static/teacher/js/room.js
+++ teacher/static/teacher/js/room.js
@@ -1,15 +1,1 @@
 
-createTestStudents = function () {
-    $.ajax({
-        url: "/teacher/test-students",
-        data: {'room_name': roomName},
-        dataType: 'json',
-        success: function () {
-            console.log("Test students created")
-        },
-        error: function (data) {
-            // TODO: remove?
-            console.log(data.error)
-        }
-    })
-};
