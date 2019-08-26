30eb3675245fa55abf4faff6b19bc754928e411b
goapunk <goapunk@riseup.net>
2019-08-26 17:16:16 +0200

    Disable autofill for usernames

--- student/forms.py
+++ student/forms.py
@@ -4,5 +4,5 @@ from teacher.models import Room
 
 
 class JoinRoomForm(forms.Form):
-    username = forms.CharField(label="Nutzername", max_length=6)
+    username = forms.CharField(label="Nutzername", max_length=6, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
     room = forms.ModelChoiceField(queryset=Room.objects.all(), to_field_name='room_name', empty_label=None)
