a4b241dde620318cfabe7878ec5b175356e2884f
bontric <benjohnwie@gmail.com>
2019-08-31 18:49:28 +0200

    fix student room choice form

--- student/forms.py
+++ student/forms.py
@@ -6,4 +6,5 @@ from teacher.models import Room
 
 class JoinRoomForm(forms.Form):
     username = forms.CharField(label="Nutzername", max_length=6, widget=forms.TextInput(attrs={'autocomplete': 'off'}))
-    room = forms.ModelChoiceField(queryset=Room.objects.filter(state=RoomStates.WAITING.value), to_field_name='room_name', empty_label=None)
+    room = forms.ModelChoiceField(queryset=Room.objects.filter(state=RoomStates.WAITING.value),
+                                  to_field_name='room_name')
