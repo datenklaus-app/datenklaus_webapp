ac03c7eec14d24088f4d9301abf71ac5dd958a38
bontric <benjohnwie@gmail.com>
2019-08-31 18:33:13 +0200

    revamp internet lesson

--- lesson/internet/internet.py
+++ lesson/internet/internet.py
@@ -5,21 +5,26 @@ from lesson.templateState import TemplateState
 
 class Internet(Lesson):
     _lessonStates = {
-        0: TemplateState("lesson/internet/initState.html", 0, 1,
-                         "Einleitung"),
-        1: TemplateState("lesson/internet/a-textCard.html", 1, 2,
+        0: TemplateState("lesson/internet/0-intro.html", 0, 1,
+                         "Einleitung", is_first=True),
+        1: TemplateState("lesson/internet/1-Hinfuerung.html", 1, 2,
                          "Hinführung zum Thema"),
-        2: TemplateState("lesson/internet/b-textCard.html", 2, 3,
+        2: TemplateState("lesson/internet/1-regel-1.html", 2, 3,
+                         "1. Regel des Internets"),
+        3: TemplateState("lesson/internet/2-internet-basics-1.html", 4, 5,
                          "Internet Basics 1 / INTERNET"),
-        3: TemplateState("lesson/internet/c-textCard.html", 3, 4,
-                         "Internet-Basics 2 / Rechner, Laptop, Smartphone, Tablet", is_sync=True),
-        4: TemplateState("lesson/internet/d-textCard.html", 4, 5,
+        4: TemplateState("lesson/internet/2-regel-2.html", 4, 5,
+                         "2. Regel des Internets"),
+        5: TemplateState("lesson/internet/3-internet-basics-2.html", 5, 6,
+                         "Internet-Basics 2 / Rechner, Laptop, Smartphone, Tablet"),
+        6: TemplateState("lesson/internet/4-internet-basics-3.html", 6, 7,
                          "Internet-Basics 3 / WLAN oder Mobilfunk?"),
-        5: TemplateState("lesson/internet/e-textCard.html", 5, 6,
+        7: TemplateState("lesson/internet/4-regel-3.html", 7, 8,
+                         "3. Regel des Internets"),
+        8: TemplateState("lesson/internet/5-internet-basics-4.html", 8, 9,
                          "Internet-Basics 4 / Zugriff auf Webseiten, Apps oder andere Dienstleistungen"),
-        6: TemplateState("lesson/internet/f-textCard.html", 6, None, "Daten - 'Überleitung", is_final=True),
+        9: TemplateState("lesson/internet/6-hinfuerung-daten.html", 9, None, "Daten - 'Überleitung", is_final=True),
     }
-
 
     @staticmethod
     def title() -> str:
